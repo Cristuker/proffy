70dfac3c653aa19db05f2cd6cda3216ab44467ee
Cristian Silva <cristian123105@gmail.com>
2020-08-06 15:03:58 -0300

    feat: add connection route whit controller

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { ClassesController } from './controllers';
+import { ClassesController, ConnectionsController } from './controllers';
 
 const routes = Router();
 
 routes.post('/classes', ClassesController.store);
 routes.get('/classes', ClassesController.index);
+routes.post('/connections', ConnectionsController.store);
+routes.get('/connections', ConnectionsController.index);
 
 export default routes;
